e260cf5c7c51754cf9f1ab3c1006c7a082bf084e
Alexander Witthus <awitthus@asu.edu>
2023-06-24 09:25:09 -0700

    format update

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,17 +52,17 @@ skillsHeader.forEach((el) =>{
 
 /*==================== EXPERIENCE TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+    tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab =>{
     tab.addEventListener('click', () =>{
         const target = document.querySelector(tab.dataset.target)
-
+        
         tabContents.forEach(tabContent =>{
             tabContent.classList.remove('experience__active')
         })
         target.classList.add('experience__active')
-
+        
         tabs.forEach(tab =>{
             tab.classList.remove('experience__active')
         })
